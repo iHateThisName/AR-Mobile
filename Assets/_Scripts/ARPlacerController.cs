6ddceb8328d31845ade0a5ea8e9b51576deaa8ee
iHateThisName <ivarravi0@gmail.com>
2026-04-29 18:56:39 +0200

    Using the new input system instead of the old one.

--- Assets/_Scripts/ARPlacerController.cs
+++ Assets/_Scripts/ARPlacerController.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.EnhancedTouch;
 using UnityEngine.XR.ARFoundation;
 using UnityEngine.XR.ARSubsystems;
 
@@ -13,20 +15,43 @@ public class ARPlacerController : MonoBehaviour {
         if (this.raycastManager == null) Debug.LogError("ARRaycastManager is not assigned.");
     }
 
+    private void OnEnable() {
+        EnhancedTouchSupport.Enable();
+    }
+
+    private void OnDisable() {
+        EnhancedTouchSupport.Disable();
+    }
+
     private void Update() {
+        if (!this.raycastManager) return;
+        if (this.isPlacing) return;
+            
 
-        if (!this.isPlacing && (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began || Input.GetMouseButtonDown(0))) {
-            this.isPlacing = true;
-
-            if (Input.touchCount > 0) {
-                // This is for touch input
-                PlaceObject(Input.GetTouch(0).position);
-
-            } else {
-                // This is for mouse input
-                PlaceObject(Input.mousePosition);
+        if (Touchscreen.current != null) {
+            var touch = Touchscreen.current.primaryTouch;
+            if (touch.press.wasPressedThisFrame) {
+                this.isPlacing = true;
+                PlaceObject(touch.position.ReadValue());
             }
+        } else if (Mouse.current != null && Mouse.current.leftButton.wasPressedThisFrame) {
+            this.isPlacing = true;
+            PlaceObject(Mouse.current.position.ReadValue());
         }
+
+
+        //if (!this.isPlacing && (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began || Input.GetMouseButtonDown(0))) {
+        //    this.isPlacing = true;
+
+        //    if (Input.touchCount > 0) {
+        //        // This is for touch input
+        //        PlaceObject(Input.GetTouch(0).position);
+
+        //    } else {
+        //        // This is for mouse input
+        //        PlaceObject(Input.mousePosition);
+        //    }
+        //}
     }
 
     private void PlaceObject(Vector2 position) {
