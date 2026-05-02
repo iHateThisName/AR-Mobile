8608dc18cce04470b9d1be72492134d0d6b6bf36
iHateThisName <ivarravi0@gmail.com>
2026-05-02 17:07:47 +0200

    ARPlacerController, removed EnhancedTouchSupport  since replace by new Input system.

--- Assets/_Scripts/ARPlacerController.cs
+++ Assets/_Scripts/ARPlacerController.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using UnityEngine.InputSystem.EnhancedTouch;
 using UnityEngine.XR.ARFoundation;
 using UnityEngine.XR.ARSubsystems;
 
@@ -13,14 +12,6 @@ public class ARPlacerController : MonoBehaviour {
 
     private void Start() {
         if (this.raycastManager == null) Debug.LogError("ARRaycastManager is not assigned.");
-    }
-
-    private void OnEnable() {
-        EnhancedTouchSupport.Enable();
-    }
-
-    private void OnDisable() {
-        EnhancedTouchSupport.Disable();
     }
 
     private void Update() {
@@ -35,20 +26,6 @@ public class ARPlacerController : MonoBehaviour {
             this.isPlacing = true;
             PlaceObject(Mouse.current.position.ReadValue());
         }
-
-
-        //if (!this.isPlacing && (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began || Input.GetMouseButtonDown(0))) {
-        //    this.isPlacing = true;
-
-        //    if (Input.touchCount > 0) {
-        //        // This is for touch input
-        //        PlaceObject(Input.GetTouch(0).position);
-
-        //    } else {
-        //        // This is for mouse input
-        //        PlaceObject(Input.mousePosition);
-        //    }
-        //}
     }
 
     private void PlaceObject(Vector2 position) {
@@ -62,11 +39,8 @@ public class ARPlacerController : MonoBehaviour {
             Vector3 hitPosition = rayHits[0].pose.position;
             Quaternion hitRotation = rayHits[0].pose.rotation;
 
-            //Instantiate(this.raycastManager.raycastPrefab, hitPosition, hitRotation);
             GameManager.Instance.OnTap(hitPosition, hitRotation);
-
         }
-
         StartCoroutine(PlacingCooldownCoroutine());
     }
 
@@ -74,4 +48,5 @@ public class ARPlacerController : MonoBehaviour {
         yield return new WaitForSeconds(0.5f);
         this.isPlacing = false;
     }
+
 }
